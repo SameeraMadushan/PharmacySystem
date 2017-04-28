8b3631fa12437d3ee01cdfc20f4e2f3c310ff9d7
Umani <umanithamarashee@gmail.com>
2017-04-28 12:38:21 +0530

    project structure created

---
+++ PharmacyAPI/models/User.js
@@ -0,0 +1,68 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var bcrypt = require('bcrypt'); //for encrypting passwords
+
+
+// set up a mongoose models
+var UserSchema = new Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    userName: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    accountType: {
+        type: String,
+        required: true
+    },
+
+    password: {
+        type: String,
+        required: true
+    }
+
+});
+/**
+ * Encrypt the password before saving
+ */
+UserSchema.pre('save', function (next) {
+    var user = this;
+    //if pw is modify  ... eg/ pw:abc
+    if (this.isModified('password') || this.isNew) {
+        bcrypt.genSalt(10, function (err, salt) {
+            if (err) {
+                return next(err);
+            }
+            bcrypt.hash(user.password, salt, function (err, hash) {
+                if (err) {
+                    return next(err);
+                }
+                user.password = hash;
+                next();
+            });
+        });
+    } else {
+        return next();
+    }
+});
+
+/**
+ * Comparing password when logging
+ */
+UserSchema.methods.comparePassword = function (passw, cb) {
+    bcrypt.compare(passw, this.password, function (err, isMatch) {
+        if (err) {
+            return cb(err);
+        }
+        cb(null, isMatch);
+    });
+};
+
+module.exports = mongoose.model('User', UserSchema);
---
+++ PharmacyAPI/server.js
@@ -0,0 +1,55 @@
+var express=require('express');
+var path=require('path');
+var cors=require('cors');
+var bodyParser=require('body-parser');
+// var morgan      = require('morgan');
+var mongoose    = require('mongoose');
+var passport	= require('passport');
+// var jwt         = require('jwt-simple');
+var session = require('express-session');
+// const MongoStore = require('connect-mongo')(session);
+
+var config      = require('./config/database'); // get db config file
+
+var User        = require('./models/User');   // get the mongoose models
+
+var api=require('./routes/api');
+
+var port = 3000;
+
+var app=express();
+
+//view engine
+// app.set('views',path.join(__dirname,'client/views'));
+// app.set('view engine','ejs');
+// app.engine('html',require('ejs').renderFile);
+
+
+
+//body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+// Use the passport package in our application
+app.use(passport.initialize());
+
+mongoose.connect(config.database);
+
+
+app.use(session({secret: 'ssshhhhh',
+    saveUninitialized: true,
+    resave: true,
+    // store:new MongoStore({mongooseConnection:mongoose.connection})
+}));
+
+require('./config/passport')(passport);
+
+app.use(cors());
+
+
+app.use('/api',api);
+
+
+app.listen(port,function () {
+    console.log("Server started on port "+port);
+});
