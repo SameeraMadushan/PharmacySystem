5dbfe4e6603e4272d97560f49150bd6c249433b8
Rushan Edirisuriya <randika266@hotmail.com>
2017-07-02 10:29:18 +0530

    updated routes files

--- PharmacyAPI/routes/drugcategory.route.js
+++ PharmacyAPI/routes/drugcategory.route.js
@@ -4,15 +4,15 @@
 var express = require('express');
 var router = express.Router();
 var bodyParser = require('body-parser');
-var modelDrug = require('../models/drug');
+var modelDrugCategory = require('../models/drugcategory');
 
 
 router.use(bodyParser.json());
 //--------------------------------------------------------------GET Requests----------------------------
 
-router.get('/api/drugCategory', function (req, res) {
+router.get('/api/drugcategory', function (req, res) {
 
-    modelDrug.getDrugsCategory(function (err, DrugObj) {
+    modelDrugCategory.getDrugsCategory(function (err, DrugObj) {
         if(err){
             res.json(err);
             res.json({success:false, msg:'Get request Fail by route!!'});
@@ -21,32 +21,43 @@ router.get('/api/drugCategory', function (req, res) {
     })
 });
 
+//------------------------Get by id ---------------------------------------------------
+router.get('/api/drugcategory/:_id', function (req, res)  {
+
+    modelDrugCategory.getDrugCategoryById(req.params._id,function (err, DrugCategoryObj) {
+        if(err){
+            res.json(err);
+            res.json({success:false, msg:'Get request Fail by route!!'});
+        }
+        res.json(DrugCategoryObj);
+    })
+});
 //--------------------------------------------------------------POST Requests----------------------------
 
-router.post('/api/drugCategory', function (req, res) {
+router.post('/api/drugcategory', function (req, res) {
 
     var prescr = req.body;
     // res.json('asd');
-    modelDrug.addDrugCategory(prescr, function (err,DrugObj) {
+    modelDrugCategory.addDrugCategory(prescr, function (err,DrugCategoryObj) {
         if(err){
             res.json(err);
             res.json({success:false, msg:'Post request Fail by route!!'});
         }
-        res.json(DrugObj);
+        res.json(DrugCategoryObj);
     })
 });
 
 //--------------------------------------------------------------PUT Request-----------------------------
 
-router.put('/api/drugCategory/:_id', function (req, res) {
+router.put('/api/drugcategory/:_id', function (req, res) {
     var id = req.params._id;
     var drugs = req.body;
-    modelDrug.updateDrugCategory(id, drugs, {}, function (err,DrugObj) {
+    modelDrugCategory.updateDrugCategory(id, drugs, {}, function (err,DrugCategoryObj) {
         if(err){
             res.json(err);
             res.json({success:false, msg:'Put request Fail by route!!'});
         }
-        res.json(DrugObj);
+        res.json(DrugCategoryObj);
     })
 });
 
